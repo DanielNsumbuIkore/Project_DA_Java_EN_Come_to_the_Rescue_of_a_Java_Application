3a8474bddb05b3af138becdfb9dc918ba257be59
Daniel Nsumbu Ikore <danielnsumbuikore@gmail.com>
2022-05-05 14:43:54 +0200

    Ajout de commentaire dans la classe Main

--- Project02Eclipse/src/com/hemebiotech/analytics/Main.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/Main.java
@@ -4,15 +4,22 @@ package com.hemebiotech.analytics;
 import java.util.List;
 import java.util.TreeMap;
 
+/**
+ *  From the file symptoms.txt  
+ *  the Main class generates a result.out file
+ *  with its symptoms and occurrences
+ *  in alphabetical order  
+ */
+
 public class Main {
 	
 
 	public static void main(String args[]) throws Exception {
-		ReadSymptomDataFromFile file = new ReadSymptomDataFromFile("symptoms.txt");
-		AnalyticsCounter counter = new AnalyticsCounter();
-		List<String> listOfSymptom = counter.reading(file);
-		TreeMap<String, Integer> sort = counter.sorting(listOfSymptom); 
-		counter.saving(sort);
+		ReadSymptomDataFromFile file = new ReadSymptomDataFromFile("symptoms.txt"); // Instantiates the ReadSymptomDataFromFile class 
+		AnalyticsCounter counter = new AnalyticsCounter();							// Instantiates the AnalyticsCounter class
+		List<String> listOfSymptom = counter.reading(file);							// Read the file and return a list of symptoms
+		TreeMap<String, Integer> sort = counter.sorting(listOfSymptom); 			// Outputs a collection of symptoms with their occurrences in alphabetical order
+		counter.saving(sort);														// Generates a result.out file with the symptoms and their occurrences written on it
 		
 	}
 	
